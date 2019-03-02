e96e0c4fa2d76e5dfc7c2b36643a6b6248791819
ningdi1994 <ningdi1994@gmail.com>
2019-03-02 18:21:05 -0500

    update

--- zuul-server/src/main/java/com/microservice/zuul/zuulserver/MyFilter.java
+++ zuul-server/src/main/java/com/microservice/zuul/zuulserver/MyFilter.java
@@ -6,9 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.net.URI;
-import java.util.logging.LogManager;
 @Component
 public class MyFilter extends ZuulFilter {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
